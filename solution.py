017cc8e97431dd35485b9b25fed8d88eb890fa1d
kperep <kperep@gmail.com>
2020-05-19 18:28:41 +0800

    Ok, problem resolved, but code look like s**t

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 
 class CarBase:
@@ -7,6 +8,10 @@ class CarBase:
         self.photo_file_name = photo_file_name
         self.carrying = carrying
 
+    def get_photo_file_ext(self):
+        file_name, file_name_ext = os.path.splitext(self.photo_file_name)
+        return file_name_ext
+
 
 class Car(CarBase):
     car_type = 'car'
@@ -14,8 +19,8 @@ class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
-        self.passenger_seat_count = passenger_seats_count
+        self.carrying = float(carrying)
+        self.passenger_seats_count = int(passenger_seats_count)
 
 
 class Truck(CarBase):
@@ -24,8 +29,14 @@ class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
-        self.body_whl = body_whl
+        self.carrying = float(carrying)
+        try:
+            self.body_length, self.body_width, self.body_height = map(float, body_whl.split('x'))
+        except ValueError:
+            self.body_length, self.body_width, self.body_height = 0.0, 0.0, 0.0
+
+    def get_body_volume(self):
+        return self.body_length * self.body_width * self.body_height
 
 
 class SpecMachine(CarBase):
@@ -34,7 +45,7 @@ class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        self.carrying = float(carrying)
         self.extra = extra
 
 
@@ -44,13 +55,35 @@ def get_car_list(csv_filename):
         reader = csv.reader(csv_fd, delimiter=';')
         next(reader)  # пропускаем заголовок
         for row in reader:
+            row_check = True
             if len(row) > 0:
                 if row[0] == 'car':
-                    print('car')
+                    file_name, file_name_ext = os.path.splitext(row[3])
+                    if file_name_ext not in ['.jpg', '.jpeg', '.png', '.gif'] or row[1] == '':
+                        row_check = False
+                    if row_check:
+                        try:
+                            car_list.append(Car(row[1], row[3], float(row[5]), int(row[2])))
+                        except ValueError:
+                            pass
                 elif row[0] == 'truck':
-                    print('truck')
+                    file_name, file_name_ext = os.path.splitext(row[3])
+                    if file_name_ext not in ['.jpg', '.jpeg', '.png', '.gif'] or row[1] == '':
+                        row_check = False
+                    if row_check:
+                        try:
+                            car_list.append(Truck(row[1], row[3], float(row[5]), row[4]))
+                        except ValueError:
+                            pass
                 elif row[0] == 'spec_machine':
-                    print('spec_machine')
+                    file_name, file_name_ext = os.path.splitext(row[3])
+                    if file_name_ext not in ['.jpg', '.jpeg', '.png', '.gif'] or row[1] == '' or row[6] == '':
+                        row_check = False
+                    if row_check:
+                        try:
+                            car_list.append(SpecMachine(row[1], row[3], float(row[5]), row[6]))
+                        except ValueError:
+                            pass
                 else:
-                    print('unknown')
+                    pass
     return car_list
