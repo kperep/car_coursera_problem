b1168bc47ba5378a946ee25fdff4909df348ba18
kperep <kperep@gmail.com>
2020-05-19 16:22:00 +0800

    Add import rows from csv

--- location.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-csv_filename = 'coursera_week3_cars.csv'
-with open(csv_filename) as csv_fd:
-    reader = csv.reader(csv_fd, delimiter=';')
-    next(reader)  # пропускаем заголовок
-    for row in reader:
-        print(row)
--- solution.py
+++ solution.py
@@ -1,23 +1,56 @@
+import csv
+
+
 class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
-        pass
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
 
 
 class Car(CarBase):
+    car_type = 'car'
+
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
-        pass
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
+        self.passenger_seat_count = passenger_seats_count
 
 
 class Truck(CarBase):
+    car_type = 'truck'
+
     def __init__(self, brand, photo_file_name, carrying, body_whl):
-        pass
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
+        self.body_whl = body_whl
 
 
 class SpecMachine(CarBase):
+    car_type = 'spec_machine'
+
     def __init__(self, brand, photo_file_name, carrying, extra):
-        pass
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
+        self.extra = extra
 
 
 def get_car_list(csv_filename):
     car_list = []
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            if len(row) > 0:
+                if row[0] == 'car':
+                    print('car')
+                elif row[0] == 'truck':
+                    print('truck')
+                elif row[0] == 'spec_machine':
+                    print('spec_machine')
+                else:
+                    print('unknown')
     return car_list
